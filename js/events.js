0c218121659dbbbf726cc149b78b468ae3645f52
DevMazed04 <mdmazedislam2684@gmail.com>
2022-08-16 10:10:28 +0600

    button onclick in different ways

--- js/events.js
+++ js/events.js
@@ -4,14 +4,29 @@ function showGreen() {
     document.body.style.backgroundColor = 'green';
 }
 
-// getMyButton.setAttribute("style", "cursor:pointer");
-// getMyButton.setAttribute("onclick", "document.body.style.backgroundColor ='wheat'");
 
+
+/* ---------------------- option-3: direct whole fuction ---------------------- */
+const getWheatButton = document.getElementById('show-wheat');
+getWheatButton.onclick = function wheatBackground() {
+    document.body.style.backgroundColor = 'wheat';
+}
+/* ----------- option-3.5 just function name with no call ----------- */
+const getPurpleButton = document.getElementById('show-purple');
+getPurpleButton.onclick = purpleBackground;
+function purpleBackground() {
+    document.body.style.backgroundColor = 'purple';
+}
+
+
+
+/* ---------------------- option-4 (mine--setAttribute) ---------------------- */
 const getMyButton = document.getElementById('show-yellow');
 getMyButton.style.cursor = "pointer";
 
+// getMyButton.setAttribute("onclick", "document.body.style.backgroundColor ='wheat'");
 getMyButton.setAttribute("onclick", "myFunction()");
 function myFunction() {
-    document.body.style.color = "brown";
+    // document.body.style.color = "brown";
     document.body.style.backgroundColor = "yellow";
 }
