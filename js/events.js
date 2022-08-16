d55dd38038186fe8acd9a967fb9d21d9f17a05a4
DevMazed04 <mdmazedislam2684@gmail.com>
2022-08-16 09:28:09 +0600

    button onclick

--- js/events.js
+++ js/events.js
@@ -1,1 +1,17 @@
 console.log("welcome!!! js dom events");
+
+function showGreen() {
+    document.body.style.backgroundColor = 'green';
+}
+
+// getMyButton.setAttribute("style", "cursor:pointer");
+// getMyButton.setAttribute("onclick", "document.body.style.backgroundColor ='wheat'");
+
+const getMyButton = document.getElementById('show-yellow');
+getMyButton.style.cursor = "pointer";
+
+getMyButton.setAttribute("onclick", "myFunction()");
+function myFunction() {
+    document.body.style.color = "brown";
+    document.body.style.backgroundColor = "yellow";
+}
